22e7ae48c3cc225ed58aec499dd265b8956705ad
Michael Meoak <mmeoak240@gmail.com>
2022-06-22 12:53:17 -0700

    commit tree 93bc3d3e4f3daf1101fefc374573669672f16be3

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+   @@artists.each_with_object([]) do |e, arr|
+    arr << e unless arr.include?(e)
+   end
+  end
+
+  def self.genres
+    @@genres.each_with_object([]) do |e, arr|
+     arr << e unless arr.include?(e)
+    end
+   end
+
+   def self.genre_count
+    @@genres.each_with_object(Hash.new(0)) do |element, hash|
+      hash[element] += 1
+   end
+   end
+
+   def self.artist_count
+    @@artists.each_with_object(Hash.new(0)) do |element, hash|
+      hash[element] += 1
+   end
+   end
+
+
+end
